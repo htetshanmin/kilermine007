41ccd735dba28ddffe1fd55140362947f4825b1f
htetshanmin <74507712+htetshanmin@users.noreply.github.com>
2020-12-15 15:25:15 +0630

    Add files via upload

---
+++ inbound1278670123516654053.py
@@ -0,0 +1,11 @@
+from random import *
+user_password = input("Enter your password: ")
+password=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i ', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', ' r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
+guess =" "
+while (guess !=user_password):
+    	guess =" "
+    	for letter in range (len(user_password)):
+    		guess_letter = password[randint(0,25)]
+    		guess =guess_letter + guess
+    		print(guess)
+    		print("Your password is",guess)
